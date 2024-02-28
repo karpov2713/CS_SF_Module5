d315da4efafbd453cb5b914c620d919f8b2ad701
karpov2713 <karpov2713@gmail.com>
2024-02-28 20:48:53 +0300

    Добавил проект по главе 5.4 - области видимости

---
+++ 5_4 FieldOfView/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_4_FieldOfView
+{
+    internal class Program
+    {
+        static int num1 = 667; // Переменная контекста класса
+
+        static int Sum()
+        {
+            var num2 = 1;
+            return num2 + num1;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(num1);
+            Console.WriteLine(Sum());
+
+            if (num1 > 0)
+            {
+                var num3 = 5;
+                Console.WriteLine(num1 + num3);
+            }
+        }
+    }
+}
