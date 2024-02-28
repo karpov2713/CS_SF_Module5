d7c70425fc0f6022ec8e22ae2796968802b75ee7
karpov2713 <karpov2713@gmail.com>
2024-02-28 21:07:32 +0300

    Глава 5.5. Рекурсия. Начало.

---
+++ 5.5 РекурсивныеФункции/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5._5_РекурсивныеФункции
+{
+    internal class Program
+    {
+        static void Echo(string saidword)
+        {
+            Console.WriteLine(saidword);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Напишите что-то: ");
+
+            var str = Console.ReadLine();
+
+            Console.Write("Укажите глубину эха: ");
+
+            var deep = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < deep; i++)
+            {
+                Echo(str);
+            }
+        }
+    }
+}
--- 5_4 FieldOfView/Program.cs
+++ 5_4 FieldOfView/Program.cs
@@ -12,20 +12,39 @@ namespace _5_4_FieldOfView
 
         static int Sum()
         {
-            var num2 = 1;
+            var num2 = 1; // Переменная контекста метода
             return num2 + num1;
         }
 
+        int val = 8;
+
+        /*static void Doubled(ref int val)
+        {
+            val *= 2;
+        }*/
+
         static void Main(string[] args)
         {
-            Console.WriteLine(num1);
+            /*Console.WriteLine(num1);
             Console.WriteLine(Sum());
 
             if (num1 > 0)
             {
                 var num3 = 5;
-                Console.WriteLine(num1 + num3);
+                Console.WriteLine(num1 + num3); // num3 - переменная контекста блока кода.
             }
+*/
+
+            /*int val = 10;
+            Doubled(ref val);
+            Console.WriteLine(val);*/
+
+            int val = 9;
+            {
+                val++;
+            }
+
+            Console.WriteLine(val);
         }
     }
 }
