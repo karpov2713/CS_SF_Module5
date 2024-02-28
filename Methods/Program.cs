7fc3c1eefdbc5cab660b12b0b23ffaeb46edc8ba
karpov2713 <karpov2713@gmail.com>
2024-02-28 13:33:50 +0300

    Добавил практику с кортежем. Повторение.

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -10,7 +10,18 @@ namespace Methods
     {
         static void Main(string[] args)
         {
-            (string Name, string[] Dishes) User;
+            /*(string Name, string[] Dishes) User;
+
+            Console.WriteLine("Введите имя пользователя: ");
+            User.Name = Console.ReadLine();
+
+            User.Dishes = new string[5];
+            for (int i = 0; i < User.Dishes.Length; i++)
+            {
+                Console.WriteLine("Введите любимое блюдо номер {0}", i + 1);
+                User.Dishes[i] = Console.ReadLine();
+            }*/
+
 
         }
     }
